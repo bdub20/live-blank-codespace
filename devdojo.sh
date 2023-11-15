decc875ec355bcbe3c8adf5db19154fd88207f3d
bdub20 <38527210+bdub20@users.noreply.github.com>
2023-11-15 06:08:45 +0000

    comments and stuff

--- devdojo.sh
+++ devdojo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# ask the user for their name
 read -p "what is your name?" name
 
+# greet the user
 echo "Hi there $name"
 echo "welcome to Gisc605"
 echo "have a great wednesday night!"
+
 
 
